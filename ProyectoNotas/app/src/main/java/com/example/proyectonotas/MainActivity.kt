1ff51713229a469d7a305f746e59e1d546c0480f
JovanyMolina <s20120208@alumnos.itsur.edu.mx>
2023-10-04 12:07:00 -0600

    barra de abajo y iconos a su posicion

--- ProyectoNotas/app/src/main/java/com/example/proyectonotas/MainActivity.kt
+++ ProyectoNotas/app/src/main/java/com/example/proyectonotas/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.proyectonotas.componentes.BottomBar
 import com.example.proyectonotas.componentes.FAB
 import com.example.proyectonotas.componentes.ToolBar
 import com.example.proyectonotas.screens.CardView
@@ -43,7 +44,8 @@ class MainActivity : ComponentActivity() {
 fun ViewContainer() {
     Scaffold (
         topBar = { ToolBar() },
-        content = { Content()}
+        content = { Content()},
+        bottomBar = {BottomBar()}//poner la barra de abajo
         //floatingActionButton = { FAB() }
     )
 }
