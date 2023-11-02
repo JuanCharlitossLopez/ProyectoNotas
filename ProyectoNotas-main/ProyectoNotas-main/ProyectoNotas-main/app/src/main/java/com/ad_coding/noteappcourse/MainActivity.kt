37f259f2aca517b15f255220acc25292d337efae
Yulisa Ramírez Rodríguez <s20120218@alumnos.itsur.edu.mx>
2023-11-02 12:06:12 -0600

    Se agrego el ViewModel y el funcionamiento de la base de datos

---
+++ ProyectoNotas-main/ProyectoNotas-main/ProyectoNotas-main/app/src/main/java/com/ad_coding/noteappcourse/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.ad_coding.noteappcourse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ad_coding.noteappcourse.ui.screen.note.NoteScreen
+import com.ad_coding.noteappcourse.ui.screen.note.NoteViewModel
+import com.ad_coding.noteappcourse.ui.screen.note_list.NoteListScreen
+import com.ad_coding.noteappcourse.ui.screen.note_list.NoteListViewModel
+import com.ad_coding.noteappcourse.ui.theme.NoteAppCourseTheme
+import com.ad_coding.noteappcourse.ui.util.Route
+import com.ad_coding.noteappcourse.ui.util.UiEvent
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteAppCourseTheme {
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = Route.noteList
+                ) {
+                    composable(route = Route.noteList) {
+                        val viewModel = hiltViewModel<NoteListViewModel>()
+                        val noteList by viewModel.noteList.collectAsStateWithLifecycle()
+
+                        NoteListScreen(
+                            noteList = noteList,
+                            onNoteClick = {
+                                navController.navigate(
+                                    Route.note.replace(
+                                        "{id}",
+                                        it.id.toString()
+                                    )
+                                )
+                            },
+                            onAddNoteClick = {
+                                navController.navigate(Route.note)
+                            }
+                        )
+                    }
+
+                    composable(route = Route.note) {
+                        val viewModel = hiltViewModel<NoteViewModel>()
+                        val state by viewModel.state.collectAsStateWithLifecycle()
+
+                        LaunchedEffect(key1 = true) {
+                            viewModel.event.collect { event ->
+                                when (event) {
+                                    is UiEvent.NavigateBack -> {
+                                        navController.popBackStack()
+                                    }
+
+                                    else -> Unit
+                                }
+                            }
+                        }
+
+                        NoteScreen(
+                            state = state,
+                            onEvent = viewModel::onEvent
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
