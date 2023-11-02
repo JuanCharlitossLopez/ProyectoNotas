79e734c093b0126ed4015980be75c0357a9376fa
Yulisa Ramírez Rodríguez <s20120218@alumnos.itsur.edu.mx>
2023-11-02 12:17:57 -0600

    Se agregó el ViewModel y el funcionamiento de la BD

--- ProyectoNotas-main/ProyectoNotas-main/ProyectoNotas-main/app/src/main/java/com/ad_coding/noteappcourse/ui/theme/Shape.kt
+++ ProyectoNotas-main/ProyectoNotas-main/ProyectoNotas-main/app/src/main/java/com/ad_coding/noteappcourse/ui/theme/Shape.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
-
+//Shapes a usar
 // Agregar las esquinas redondeadas
 val Shapes = Shapes (
     small = RoundedCornerShape(4.dp),
